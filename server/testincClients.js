3478e7aaa75a938bc2fed1a58bc7a4d1f41ec756
Valter Santos Matos <valter.santos.matos@gmail.com>
2015-03-21 20:35:54 +0800

    parseClient v1

--- server/testincClients.js
+++ server/testincClients.js
@@ -1,7 +1,39 @@
 
 
 
-var m = require('./facebookClient/facebookClient');
-m.myFunc1();
+var parseClient = require('./parseClient/parseClient');
+
+//parseClient.saveUser("bambi3", "tahaFbKeyyyyyy").then(function(user){
+//    console.log(user.id)
+//  }
+//)
+
+
+
+parseClient.getUser("bambi").then(function(user){
+
+  parseClient.getEvents(user.id).then(function(results){
+
+    for (var i = 0; i < results.length; i++) {
+      var object = results[i];
+      console.log(object.id + ' - ' + object.get('name'));
+    }
+
+  })
+
+  //var fbEventId = "first event!";
+  //var listOfFriendsInvited = ["bambi", "taha"];
+  //var name = "masterCardHackaton";
+  //var description = "dinner";
+  //var listOfFriendsThatHavePaid = [];
+  //
+  //parseClient.saveEvent(fbEventId,listOfFriendsInvited,name,description,listOfFriendsThatHavePaid,user.id).then(function(event){
+  //
+  //  console.log(event.id);
+  //})
+
+})
+
+
 
 //dasadsdsad
